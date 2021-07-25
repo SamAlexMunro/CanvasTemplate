c40928b66bd622590d5af5da2ad9948842f2bf44
Sam M <emberdesignltd@protonmail.com>
2021-07-25 14:42:52 +0100

    feat(App): Clean up/refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CanvasService } from './canvasUtils/canvas.service';
+import { CanvasContext } from './enums/canvas-context';
 import { MouseService } from './services/mouse.service';
 import { ResizeService } from './services/resize.service';
 
@@ -10,13 +11,41 @@ import { ResizeService } from './services/resize.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  canvasElement = this.canvasService.createNewCanvasElement('');
+  canvasRenderer = this.canvasElement.getContext(
+    CanvasContext.TWO_DIMENSIONAL
+  ) as CanvasRenderingContext2D;
+
   constructor(
     private readonly canvasService: CanvasService,
     private readonly mouseService: MouseService,
     private readonly resizeService: ResizeService
   ) {
-    this.mouseService.mouseCoordinates.subscribe(console.log);
-    this.canvasService.createNewCanvasElement('test');
-    this.resizeService.resize$.subscribe(console.log);
+    // Mouse coordinates {x: 0, y: 0}, remove if not needed
+    this.mouseService.mouseCoordinates$.subscribe(console.log);
+
+    if (!this.canvasRenderer) return;
+    this.canvasResizeListener();
+    this.animate();
+  }
+
+  private canvasResizeListener(): void {
+    /**
+     * If a canvas isn't persistant throughout apps lifetime add unsubscription logic
+     * recommend takeUntil or async pipe
+     */
+    this.resizeService.resize$.subscribe((event) => {
+      this.canvasElement.width = event.width;
+      this.canvasElement.height = event.height;
+      this.draw();
+    });
+  }
+
+  private draw(): void {
+    // Drawing logic
+  }
+
+  private animate(): void {
+    requestAnimationFrame(this.animate.bind(this));
   }
 }
