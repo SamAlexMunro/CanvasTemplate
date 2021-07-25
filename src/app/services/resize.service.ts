0ffce5cf5c237a8ea1cdfb31ad2bd4ec8feaf0fb
Sam M <emberdesignltd@protonmail.com>
2021-07-25 14:42:06 +0100

    feat(ResizeService): Clean up/refactor

--- src/app/services/resize.service.ts
+++ src/app/services/resize.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import { fromEvent } from 'rxjs';
-import { map, throttleTime } from 'rxjs/operators';
+import { fromEvent, Observable } from 'rxjs';
+import { map, startWith, throttleTime } from 'rxjs/operators';
 import { WindowEvent } from './../enums/window-events';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class ResizeService {
-  resize$ = fromEvent(window, WindowEvent.RESIZE).pipe(
+  resize$: Observable<{ width: number; height: number }> = fromEvent(
+    window,
+    WindowEvent.RESIZE
+  ).pipe(
+    startWith(window.innerWidth),
     throttleTime(50),
-    map(() => window.innerWidth)
+    map(() => ({ width: window.innerWidth, height: window.innerHeight }))
   );
 }
