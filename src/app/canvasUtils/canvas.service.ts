eb8443edbc58b6aaa31d886e75cbc578264f13d3
Sam M <emberdesignltd@protonmail.com>
2021-07-25 14:41:41 +0100

    feat(CanvasService): Clean up/refactor

--- src/app/canvasUtils/canvas.service.ts
+++ src/app/canvasUtils/canvas.service.ts
@@ -6,22 +6,25 @@ const CANVAS_DEFAULT_CLASS = 'canvas-boiler-plate';
 
 @Injectable({ providedIn: 'root' })
 export class CanvasService {
-  renderer = this.rendererFactory2.createRenderer(null, null);
+  private readonly renderer = this.rendererFactory2.createRenderer(null, null);
+
   constructor(private readonly rendererFactory2: RendererFactory2) {}
 
-  createNewCanvasElement(id = ''): HTMLCanvasElement | undefined {
+  createNewCanvasElement(id = ''): HTMLCanvasElement {
     const canvasElement = this.renderer.createElement(EHtmlElement.CANVAS);
     const appRoot = document.getElementsByTagName(EHtmlElement.APP_ROOT)[0];
     this.renderer.setAttribute(canvasElement, HtmlAttribute.ID, id);
     this.renderer.addClass(canvasElement, CANVAS_DEFAULT_CLASS);
+
     if (!appRoot) {
       console.error('app-root element was not found!');
-      return;
+      return canvasElement;
     }
+
     this.renderer.appendChild(appRoot, canvasElement);
     return canvasElement;
   }
 
-  // Unlikely to be used but use as a template if required to add prototype methods to a given canvas element.
-  // canvasElement.prototype.removeCanvasElement = () => {};
+  // Would reccomend adding protoype properties if multiple canvas's are being created.
+  // canvasElement.prototype.methodName = () => { logic... };
 }
